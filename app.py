c6f57d2e25717f3289c2746f55306d14704ce122
Jainnielyn Ching-Gong <jniching1@yahoo.com>
2020-09-02 08:45:25 -0400

    Drafted first page

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template, url_for
+import requests, json
+import random, config
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == "POST":
+        return "Starting game"
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
