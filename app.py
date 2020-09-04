a2ce43b966e7473483baf32a23ab15d2d2d8ca29
Jainnielyn Ching <missjainnie@gmail.com>
2020-09-04 10:12:46 -0400

    Successfully gets list of spells from API

--- app.py
+++ app.py
@@ -5,11 +5,26 @@ import config
 
 app = Flask(__name__)
 
+def get_questions(num):
+    # Get all spells from API
+    response = requests.get(f"https://www.potterapi.com/v1/spells?key={config.api_key}")
+    spells = response.json()
+
+    # Get num amount of spell questions, and multiple choice options
+    test_spells = random.sample(spells, num)
+    options = random.sample(spells, num*3)
+    options = [o['spell'] for o in options]
+
+    return test_spells, options
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
-        num = request.form['num']
-        return "Starting game"
+        num = int(request.form['num'])
+        #return f"Starting game with {num} questions"
+        test_spells, options = get_questions(num)
+        print(test_spells)
+        return "Printed test spells"
     else:
         return render_template("index.html")
 
